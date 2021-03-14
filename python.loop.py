2d26ff9695ca56236d12d928465a87a704dd40de
shdbwa <kronos.shorty@gmail.com>
2021-03-14 09:09:58 +0100

    Create python.loop.py

---
+++ python.loop.py
@@ -0,0 +1,17 @@
+# Print each fruit in a fruit list:
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  
+  
+# Loop through the letters in the word "banana":
+for x in "banana":
+print(x)
+
+
+# Exit the loop when x is "banana":
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
